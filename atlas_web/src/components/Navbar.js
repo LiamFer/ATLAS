39176d16bee8428cd4eb2ad0eb9a8329e6983d92
William Fernandes <wiilfern1910@outlook.com>
2024-08-10 12:21:51 -0300

    Finished Header

--- atlas_web/src/components/Navbar.js
+++ atlas_web/src/components/Navbar.js
@@ -1,38 +1,46 @@
 import "./navbar.css";
-import { useState } from "react";
+import { Component, useState } from "react";
 import { Divide as Hamburger } from "hamburger-react";
 
-function Navbar() {
-  //const [variable, setVariable] = useState();
-  return (
-    <>
-      <nav>
-        <a id="logo" href="index.html">
-          ATLAS
-        </a>
-        <div>
-          <ul id="navbar">
-            <li>
-              <a className="nav-link nav-link-grow-up">Home</a>
-            </li>
-            <li>
-              <a className="nav-link nav-link-grow-up">About</a>
-            </li>
-            <li>
-              <a className="nav-link nav-link-grow-up">Download</a>
-            </li>
-            <li>
-              <a className="nav-link nav-link-grow-up">Contact</a>
-            </li>
-          </ul>
-        </div>
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-        <div id="mobile-menu">
-          <Hamburger color="white"/>
-        </div>
+  render() {
+    return (
+      <>
+        <nav>
+          <a id="logo" href="index.html">
+            ATLAS
+          </a>
+          <div>
+            <ul
+              id="navbar"
+              className={this.state.clicked ? "#navbar expand" : "#navbar"}
+            >
+              <li>
+                <a className="nav-link nav-link-grow-up">Home</a>
+              </li>
+              <li>
+                <a className="nav-link nav-link-grow-up">About</a>
+              </li>
+              <li>
+                <a className="nav-link nav-link-grow-up">Download</a>
+              </li>
+              <li>
+                <a className="nav-link nav-link-grow-up">Contact</a>
+              </li>
+            </ul>
+          </div>
 
-      </nav>
-    </>
-  );
+          <div id="mobile-menu" onClick={this.handleClick}>
+            <Hamburger color="white" />
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
 export default Navbar;
---
+++ atlas_web/src/components/landing.js
@@ -0,0 +1,5 @@
+import "./landing.css"
+
+function landing(){
+    
+}
