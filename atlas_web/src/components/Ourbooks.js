c3180cd3cb5da228ca9ee959487797e2c03e9390
William Fernandes <wiilfern1910@outlook.com>
2024-08-11 11:26:45 -0300

    Books Section

--- atlas_web/src/components/Ourbooks.js
+++ atlas_web/src/components/Ourbooks.js
@@ -1,13 +1,50 @@
 import { bookCovers } from "../constants";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Atropos from "atropos/react";
 import "./ourbooks.css";
 import "atropos/css";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 function Ourbooks() {
   return (
     <>
       <div id="bookssection">
-        <h1>Some of our Books</h1>
+        <h1>ATLAS LIBRARY</h1>
+        <Swiper
+          id="booksswiper"
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          style={{
+            "--swiper-pagination-color": "#23c483",
+            "--swiper-pagination-bullet-inactive-color": "#999999",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bullet-size": "16px",
+            "--swiper-pagination-bullet-horizontal-gap": "6px",
+          }}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper2"
+        >
+          {bookCovers.map((bookCover, index) => (
+            <SwiperSlide className="swiper2 swiper-slide2">
+              <img className="book-cover" src={bookCover} alt="Book Cover" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
         <div id="booksbox">
           {bookCovers.map((bookCover, index) => (
             <Atropos
