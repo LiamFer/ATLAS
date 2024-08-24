ba2d96f17605db17c2abca63bb4affd9711eb581
William Fernandes <wiilfern1910@outlook.com>
2024-08-24 20:56:01 -0300

    Building

--- nodeTesting/requesting.js
+++ nodeTesting/requesting.js
@@ -1,17 +1,20 @@
-const axios = require("axios");
-const API = "http://localhost:8090/api/readers";
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key:
+    process.env.MAILGUN_API_KEY ||
+    "ef4e9be96b73c3d99a929f847d6fc60b-2b91eb47-cf3d21f4",
+});
 
-let user = {
-  name: "catchexception",
-  email: "except"
-};
-
-// Função pra inserir o usuário no Banco de dados chamando a API usando Promises
-axios
-  .post(API, user)
-  .then((response) => {
-    console.log(`STATUS CODE IS ${response.status}`);
+mg.messages
+  .create("sandbox-123.mailgun.org", {
+    from: "mailgun@atlaslibrary.tech",
+    to: ["wiilfern1910@outlook.com"],
+    subject: "Hello",
+    text: "Testing some Mailgun awesomeness!",
+    html: "<h1>Testing some Mailgun awesomeness!</h1>",
   })
-  .catch((error) => {
-    console.log(`ERROR ${error.response.status} - ${error.response.data}`);
-  });
+  .then((msg) => console.log(msg)) // logs response data
+  .catch((err) => console.log(err)); // logs any error
